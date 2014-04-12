c895f9a272e4ee930fecf7cb97b122d0ea3aa86b
laa-1-yay <laavanye.bahl@gmail.com>
2014-04-13 01:48:23 +0530

    Added SPLASH SCREEN with song and background

---
+++ teamlrp/src/main/java/in/co/teamlrp/guessthevoice/SplashScreen.java
@@ -0,0 +1,48 @@
+package in.co.teamlrp.guessthevoice;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class SplashScreen extends Activity {
+
+
+    private final int SPLASH_DISPLAY_LENGHT = 9000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        MediaPlayer mPlayer = MediaPlayer.create(this, R.raw.letsgetstarted);
+        mPlayer.start();
+
+
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+/* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                                               /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+
+
+}
